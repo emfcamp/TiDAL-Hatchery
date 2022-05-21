20e4d2d6ef66824af416eda5709bd918626afcc5
Mark Steward <marksteward@gmail.com>
2022-05-21 23:55:26 +0100

    Changes from LWK

--- docker/laravel-prod.sh
+++ docker/laravel-prod.sh
@@ -8,7 +8,8 @@ cp -RT /app/public /yarn-out
 # Access to storage and logs
 chown -R www-data:www-data storage
 
-php artisan migrate --seed
+#php artisan migrate --seed
+php artisan migrate --force
 php artisan config:cache
 
 exec php-fpm
